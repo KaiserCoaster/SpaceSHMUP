1e8c36db4b9d19e0fd6e4ffc95884badeace7115
Matthew Kaiser <matt@kaiserdesigns.com>
2016-03-21 15:36:24 -0400

    Firing, PowerUps. etc.
    Don’t remember what was in the previous commit.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -3,32 +3,45 @@ using System.Collections;
 
 public class Hero : MonoBehaviour {
 
-	static public Hero		S;
+	static public Hero S;
 
-	public float	speed = 30;
-	public float	rollMult = -45;
-	public float  	pitchMult=30;
+	public float gameRestartDelay = 2f;
 
-	public float	shieldLevel=1;
+	public float speed = 30;
+	public float rollMult = -45;
+	public float pitchMult = 30;
+
+	[SerializeField]
+	private float _shieldLevel = 1;
+
+	// Weapon fields
+	public Weapon[] weapons;
 
 	public bool	_____________________;
 	public Bounds bounds;
 
-	void Awake(){
+	// Declare a new delegate type WeaponFireDelegate
+	public delegate void WeaponFireDelegate ();
+	// Create a WeaponFireDelegate field named fireDelegate.
+	public WeaponFireDelegate fireDelegate;
+
+	void Awake () {
 		S = this;
 		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
+
 	}
 
-
-	// Use this for initialization
 	void Start () {
-	
+		// Reset the weapons to start _Hero with 1 blaster
+		ClearWeapons ();
+		weapons [0].SetType (WeaponType.blaster);
 	}
+		
 	
 	// Update is called once per frame
 	void Update () {
-		float xAxis = Input.GetAxis("Horizontal");
-		float yAxis = Input.GetAxis("Vertical");
+		float xAxis = Input.GetAxis ("Horizontal");
+		float yAxis = Input.GetAxis ("Vertical");
 
 		Vector3 pos = transform.position;
 		pos.x += xAxis * speed * Time.deltaTime;
@@ -38,13 +51,109 @@ public class Hero : MonoBehaviour {
 		bounds.center = transform.position;
 		
 		// constrain to screen
-		Vector3 off = Utils.ScreenBoundsCheck(bounds,BoundsTest.onScreen);
+		Vector3 off = Utils.ScreenBoundsCheck (bounds, BoundsTest.onScreen);
 		if (off != Vector3.zero) {  // we need to move ship back on screen
 			pos -= off;
 			transform.position = pos;
 		}
 		
 		// rotate the ship to make it feel more dynamic
-		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
+		transform.rotation = Quaternion.Euler (yAxis * pitchMult, xAxis * rollMult, 0);
+
+		// Use the fireDelegate to fire Weapons
+		// First, make sure the Axis("Jump") button is pressed 
+		// Then ensure that fireDelegate isn't null to avoid an error
+		if (Input.GetAxis ("Jump") == 1 && fireDelegate != null) {           // 1
+			fireDelegate ();
+		}
+	}
+
+	// This variable holds a reference to the last triggering GameObject
+	public GameObject lastTriggerGo = null;
+
+	void OnTriggerEnter (Collider other) {
+		// Find the tag of other.gameObject or its parent GameObjects
+		GameObject go = Utils.FindTaggedParent (other.gameObject);
+		// If there is a parent with a tag
+		if (go != null) {
+			// Make sure it's not the same triggering go as last time
+			if (go == lastTriggerGo) {
+				return;
+			}
+			lastTriggerGo = go;
+
+			if (go.tag == "Enemy") {
+				// If the shield was triggered by an enemy
+				// Decrease the level of the shield by 1
+				shieldLevel--;
+				// Destroy the enemy
+				Destroy (go);
+			} else if (go.tag == "PowerUp") {
+				// If the shield was triggerd by a PowerUp
+				AbsorbPowerUp (go);
+			} else {
+				// Announce it
+				print ("Triggered: " + go.name);
+			}
+		} else {
+			// Otherwise announce the original other.gameObject
+			print ("Triggered: " + other.gameObject.name); // Move this line here!
+		}
+	}
+
+	public float shieldLevel {
+		get {
+			return(_shieldLevel);
+		}
+		set {
+			_shieldLevel = Mathf.Min (value, 4);
+			// If the shield is going to be set to less than zero
+			if (value < 0) {
+				// Tell Main.S to restart the game after a delay
+				Main.S.DelayedRestart (gameRestartDelay);
+				Destroy (this.gameObject);
+			}
+		}
+	}
+
+	public void AbsorbPowerUp (GameObject go) {
+		PowerUp pu = go.GetComponent<PowerUp> ();
+		switch (pu.type) {
+		case WeaponType.shield: // If it's the shield
+			shieldLevel++;
+			break;
+
+		default: // If it's any Weapon PowerUp
+			            // Check the current weapon type
+			if (pu.type == weapons [0].type) {
+				// then increase the number of weapons of this type
+				Weapon w = GetEmptyWeaponSlot (); // Find an available weapon
+				if (w != null) {
+					// Set it to pu.type
+					w.SetType (pu.type);
+				}
+			} else {
+				// If this is a different weapon
+				ClearWeapons ();
+				weapons [0].SetType (pu.type);
+			}
+			break;
+		}
+		pu.AbsorbedBy (this.gameObject);
+	}
+
+	Weapon GetEmptyWeaponSlot () {
+		for (int i = 0; i < weapons.Length; i++) {
+			if (weapons [i].type == WeaponType.none) {
+				return(weapons [i]);
+			}
+		}
+		return(null);
+	}
+
+	void ClearWeapons () {
+		foreach (Weapon w in weapons) {
+			w.SetType (WeaponType.none);
+		}
 	}
 }
